59da016de7895cf1fd7a0cd8ec6f4cf548211ee7
zygnematphyce <lfourteau91@gmail.com>
2022-02-11 17:50:40 +0100

    Fix bash typo.

--- launch.sh
+++ launch.sh
@@ -6,16 +6,15 @@ ROOT_URL="${ROOT_URL:-http://localhost}"
 
 if [[ $RUN_MODE == "dev" ]]
 then
-
   npm run dev
-
 else
-
   if [[ ! -z "$MONGO_URL" ]]
   then
     ./genenotebook_*/genenotebook run --mongo-url $MONGO_URL --root-url $ROOT_URL
   elif [[ ! -z "$DB_PATH" ]]
+  then
     ./genenotebook_*/genenotebook run --db-path $DB_PATH --root-url $ROOT_URL
   else
     ./genenotebook_*/genenotebook run --root-url $ROOT_URL
+  fi
 fi
